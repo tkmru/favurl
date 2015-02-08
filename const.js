af24d0b96378b4c4488c0a2eee6707c34d6cae16
tkmru <i.am.tkmru@gmail.com>
2015-02-09 00:24:36 +0900

    recover const.js

---
+++ const.js
@@ -0,0 +1,4 @@
+const CONSUMER_KEY = '**********';
+const CONSUMER_SECRET = '**********';
+const ACCESS_TOKEN_STORAGE_KEY = 'access token';
+const ACCESS_TOKEN_SECRET_STORAGE_KEY = 'access token secret';
